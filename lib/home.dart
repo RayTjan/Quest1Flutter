5a17224f0d473ce3571d093d619b11eb2999e47a
RayTjan <62584785+RayTjan@users.noreply.github.com>
2021-03-01 16:59:55 +0700

    update

--- lib/home.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-//material app>Scaffold>container>row>column
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
